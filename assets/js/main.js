5cb0e23c251ab1f4580b754b9477a6725807c064
Quentin <quentinlemarchand0214@gmail.com>
2017-06-27 15:45:19 +0200

    init with fullpage

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 700,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: false,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: true,
+		verticalCentered: true,
+		sectionsColor : ['#ccc', '#fff'],
+		paddingTop: '3em',
+		paddingBottom: '10px',
+		fixedElements: '#header, .footer',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.slide',
+
+		lazyLoading: true,
+
+		//events
+		onLeave: function(index, nextIndex, direction){},
+		afterLoad: function(anchorLink, index){},
+		afterRender: function(){},
+		afterResize: function(){},
+		afterResponsive: function(isResponsive){},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+	});
+});
+
+var items = document.querySelectorAll('.item');
+var container = document.querySelector('#fullpage');
+var chapters_btn = document.querySelector('.chapters');
+var chapters = document.querySelector('.menu-pop-up');
+
+for(var i = 0; i < items.length; i++)
+{
+	items[i].addEventListener('click', function(e) 
+	{
+		container.style.transform = 'translateY(-' + e.target.id * 100 + '%)';
+	});
+}
+
+chapters_btn.addEventListener('click', function(e) {
+	chapters.style.opacity = 1;
+});
+
+var stars = document.querySelectorAll('.star');
+var bars = document.querySelectorAll('.bar');
+var items = document.querySelectorAll('.timeline-items');
+
+for(var i = 0; i < stars.length; i++)
+{
+	stars[i].addEventListener('click', function(e) 
+	{
+		console.log(e.target.id);
+		this.style.width = '20px'
+		this.style.height = '20px'
+		this.style.transform = 'translateY(100%)'
+		items[parseInt(e.target.id)-1].style.transformOrigin = '0% 0%'
+		items[parseInt(e.target.id)-1].style.transform = 'rotate(10deg)'
+		items[parseInt(e.target.id)+1].style.transformOrigin = '100% 100%'
+		items[parseInt(e.target.id)+1].style.transform = 'rotate(-10deg)'
+	});
+}
